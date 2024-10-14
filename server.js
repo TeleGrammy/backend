1d2e699c2ab0980b519b143eceec8b42aac00fb2
ali-bahr <alibahr313@gmail.com>
2024-10-14 09:30:33 +0300

    modified inital files

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const app = require("./app");
 
-app.listen(process.env.PORT || 8000, process.env.HOSTNAME || "localhost");
+app.listen(process.env.PORT, process.env.HOSTNAME, () => {
+  console.log(
+    `Server is running on http://${process.env.HOSTNAME}:${process.env.PORT}`,
+  );
+});
