9d1dc00497851f926a44d8da233b33466d41ef0c
Mostafa Rabie <mostafamohammed27.mm@gmail.com>
2024-10-17 02:02:20 +0300

    style: Modify ESLint configuration and Prettier configuration files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config({
-  path: ".env"
+  path: ".env",
 });
 
 const app = express();
--- src/models/user.js
+++ src/models/user.js
@@ -5,26 +5,26 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: [true, "please enter a username"],
-    unique: [true, "the user name already used"]
+    unique: [true, "the user name already used"],
   },
   email: {
     type: String,
     required: [true, "please enter your email address"],
     unique: [true, "your email address is not correct"],
     lowercase: true,
-    validate: [validator.isEmail, "Please provide a valid email"]
+    validate: [validator.isEmail, "Please provide a valid email"],
   },
   password: {
     type: String,
-    required: [true, "please enter a password"]
+    required: [true, "please enter a password"],
   },
   phone: {
     type: String,
-    unique: true
+    unique: true,
   },
   registrationDate: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   picture: {type: String},
   bio: {type: String},
@@ -32,8 +32,8 @@ const userSchema = new mongoose.Schema({
   status: {
     type: String,
     enum: ["active", "inactive", "banned"],
-    default: "inactive"
-  }
+    default: "inactive",
+  },
 });
 
 const User = mongoose.model("User", userSchema);
