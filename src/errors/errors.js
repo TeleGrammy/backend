73dd69b50075833a0a71cd4463f6bc2c41faabe5
Mostafa Rabie <mostafamohammed27.mm@gmail.com>
2024-10-18 20:28:40 +0300

    feat: add cast error handler

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -1,8 +1,10 @@
 const validationErrorHandler = require("./validationErrorHandler");
 const duplicateKeyErrorHandler = require("./duplicateKeyErrorHandler");
+const castErrorHandler = require("./castErrorHandler");
 
 module.exports = (err) => {
   if (err.name === "ValidationError") return validationErrorHandler(err);
   if (err.code === 11000) return duplicateKeyErrorHandler(err);
+  if (err.name === "CastError") return castErrorHandler(err);
   return err;
 };
