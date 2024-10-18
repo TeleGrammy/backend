d44c15699537c5cc4c361a058ffce48be613cb9e
Mostafa Rabie <mostafamohammed27.mm@gmail.com>
2024-10-18 20:55:04 +0300

    feat: add authentication routes

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ require("dotenv").config({
 });
 
 const globalErrorHandler = require("./src/middlewares/globalErrorHandling");
+const authenticationRoutes = require("./src/routes/authenticationRoutes");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/api/v1/auth", authenticationRoutes);
 
 app.use(globalErrorHandler);
 
