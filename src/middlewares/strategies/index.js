0b2dcfdf1a8f07b80364693b8efcb3b1c4be322a
Abdulrahman-Mostafa10 <abdelrahman.abdeltawab03@eng-st.cu.edu.eg>
2024-10-31 05:13:39 +0300

    [⛔DELETED]: Deleted FaceBook Authentication

--- src/middlewares/strategies/index.js
+++ src/middlewares/strategies/index.js
@@ -1,5 +1,4 @@
 const googleStrategy = require("./googleStrategy");
 const githubStrategy = require("./githubStrategy");
-const faceBookStrategy = require("./facebookStrategy");
 
-module.exports = {googleStrategy, githubStrategy, faceBookStrategy};
+module.exports = {googleStrategy, githubStrategy};
