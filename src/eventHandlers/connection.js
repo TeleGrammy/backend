a371d09f46edfbf7b69a010d61fc2a0962e1197f
ali-bahr <alibahr313@gmail.com>
2024-11-02 14:13:40 +0200

    [fix]: fix error in requiring function from eventHandler

--- src/eventHandlers/connection.js
+++ src/eventHandlers/connection.js
@@ -1,1 +1,3 @@
-module.exports.onConnection = async (socket) => {};
+module.exports.onConnection = async (socket) => {
+  console.log(`New connection: ${socket.id}`);
+};
--- src/ioApp.js
+++ src/ioApp.js
@@ -1,9 +1,8 @@
 const {Server} = require("socket.io");
-const onConnection = require("./eventHandlers/connection");
+const {onConnection} = require("./eventHandlers/connection");
 
 const createIoApp = (httpServer) => {
   const io = new Server(httpServer);
-
   io.on("connection", onConnection);
 };
 
