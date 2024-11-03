4458864abb08db77793b76e2fd656592376d8ade
Mostafa Rabie <114685686+mostafarabie5@users.noreply.github.com>
2024-11-03 23:28:42 +0200

    fix: fix redirect url for googe and github (#35)
    * fix: fix redirect url for googe and github
    
    * fix: fix login callback url

--- src/controllers/auth/googleAuthController.js
+++ src/controllers/auth/googleAuthController.js
@@ -53,7 +53,11 @@ const googleCallBack = catchAsync(async (req, res, next) => {
       const {updatedUser, accessToken} =
         await manageSessionForUserModule.default(req, res, existingUser);
 
-      return res.status(300).redirect(process.env.FRONTEND_LOGIN_CALLBACK);
+      return res
+        .status(300)
+        .redirect(
+          process.env.FRONTEND_LOGIN_CALLBACK + `?accessToken=${accessToken}`
+        );
     }
   )(req, res);
 });
