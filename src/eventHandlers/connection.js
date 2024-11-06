de3d179aa52131d5d18459d52b512d50cafe1b37
Mohamed Abdelmeged <121584524+Mo-Naga@users.noreply.github.com>
2024-11-06 16:38:41 +0200

    adding authentication

--- src/eventHandlers/connection.js
+++ src/eventHandlers/connection.js
@@ -11,14 +11,14 @@ const {updateTypingStatus} = require("./chat/typing");
 exports.onConnection = async (socket, io) => {
   console.log("User connected:", socket.id);
 
-  const userId = socket.handshake.query.id;
+  const userId = socket.user.id;
 
   socket.userId = userId;
   console.log("User id connected:", userId);
-
   /**  user will join all channels once connected so there will be not a join chat event */
   // user join it is own room
   socket.join(`${userId}`);
+
   const {contacts} = await userService.getUserByID(socket.userId);
   contacts.forEach((contact) => socket.join(`chat:${contact.chatId}`));
 
@@ -30,7 +30,12 @@ exports.onConnection = async (socket, io) => {
 
   socket.on("typing", updateTypingStatus({io, socket}));
 
+  socket.on("message", (msg) => {
+    console.log("Message from server:", msg);
+  });
+
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
   });
+  
 };
