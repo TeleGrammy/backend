b7e169ac7142cd24b72088d7531314ba53bfe1b3
Mostafa Rabie <mostafamohammed27.mm@gmail.com>
2024-11-26 01:14:28 +0200

    feat: add group schema

---
+++ src/models/groupModel.js
@@ -0,0 +1,264 @@
+const mongoose = require("mongoose");
+
+const memberSchema = new mongoose.Schema({
+  memberId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  joinedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  leftAt: {
+    type: Date,
+    default: null,
+  },
+  mute: {
+    type: Boolean,
+    default: false,
+  },
+  muteUntil: {
+    type: Date,
+    default: null,
+  },
+  permission: {
+    sendMessages: {
+      type: Boolean,
+    },
+    sendMedia: {
+      type: {
+        photos: {
+          type: Boolean,
+          default: true,
+        },
+        videos: {
+          type: Boolean,
+          default: true,
+        },
+        files: {
+          type: Boolean,
+          default: true,
+        },
+        music: {
+          type: Boolean,
+          default: true,
+        },
+        voiceMessages: {
+          type: Boolean,
+          default: true,
+        },
+        videoMessages: {
+          type: Boolean,
+          default: true,
+        },
+        stickers: {
+          type: Boolean,
+          default: true,
+        },
+        polls: {
+          type: Boolean,
+          default: true,
+        },
+        embedLinks: {
+          type: Boolean,
+          default: true,
+        },
+      },
+    },
+    addUsers: {
+      type: Boolean,
+      default: true,
+    },
+    pinMessages: {
+      type: Boolean,
+      default: true,
+    },
+    changeChatInfo: {
+      type: Boolean,
+      default: true,
+    },
+  },
+});
+
+const adminSchema = new mongoose.Schema({
+  adminId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  joinedAt: {
+    type: Date,
+  },
+  leftAt: {
+    type: Date,
+    default: null,
+  },
+  AdminAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  mute: {
+    type: Boolean,
+    default: false,
+  },
+  muteUntil: {
+    type: Date,
+    default: null,
+  },
+  customTitle: {
+    type: String,
+    default: "Admin",
+  },
+  permission: {
+    changeGroupInfo: {
+      type: Boolean,
+      default: true,
+    },
+    deleteMessages: {
+      type: Boolean,
+      default: true,
+    },
+    banUsers: {
+      type: Boolean,
+      default: true,
+    },
+    addUsers: {
+      type: Boolean,
+      default: true,
+    },
+    inviteUsersViaLink: {
+      type: Boolean,
+      default: true,
+    },
+    pinMessages: {
+      type: Boolean,
+      default: true,
+    },
+    postStories: {
+      type: Boolean,
+      default: true,
+    },
+    editStories: {
+      type: Boolean,
+      default: true,
+    },
+    deleteStories: {
+      type: Boolean,
+      default: true,
+    },
+    manageLiveStreams: {
+      type: Boolean,
+      default: true,
+    },
+    addNewAdmins: {
+      type: Boolean,
+      default: true,
+    },
+    remainAnonymous: {
+      type: Boolean,
+      default: true,
+    },
+  },
+});
+
+const groupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "The group name is required..."],
+  },
+  image: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  groupType: {
+    type: String,
+    enum: ["Public", "Private"],
+    default: "Private",
+  },
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "The owner id is required."],
+  },
+  groupPermission: {
+    type: {
+      sendTextMessages: {
+        type: Boolean,
+        default: true,
+      },
+      sendMedia: {
+        type: {
+          photos: {
+            type: Boolean,
+            default: true,
+          },
+          videos: {
+            type: Boolean,
+            default: true,
+          },
+          files: {
+            type: Boolean,
+            default: true,
+          },
+          music: {
+            type: Boolean,
+            default: true,
+          },
+          voiceMessages: {
+            type: Boolean,
+            default: true,
+          },
+          videoMessages: {
+            type: Boolean,
+            default: true,
+          },
+          stickers: {
+            type: Boolean,
+            default: true,
+          },
+          polls: {
+            type: Boolean,
+            default: true,
+          },
+          embedLinks: {
+            type: Boolean,
+            default: true,
+          },
+        },
+      },
+      addUsers: {
+        type: Boolean,
+        default: true,
+      },
+      pinMessages: {
+        type: Boolean,
+        default: true,
+      },
+      changeChatInfo: {
+        type: Boolean,
+        default: true,
+      },
+    },
+  },
+  groupSizeLimit: {
+    type: Number,
+    default: 200000,
+  },
+  members: {
+    type: [memberSchema],
+  },
+  admins: {
+    type: [adminSchema],
+  },
+});
+
+const Group = mongoose.model("Group", groupSchema);
+const GroupAdmin = mongoose.model("GroupAdmin", adminSchema);
+const GroupMember = mongoose.model("GroupMember", memberSchema);
+
+module.exports = {
+  Group,
+  GroupAdmin,
+  GroupMember,
+};
