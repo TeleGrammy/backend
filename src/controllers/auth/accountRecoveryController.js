97eebbcb38972d1113ebc68009ed30977415f4ff
Mostafa Rabie <114685686+mostafarabie5@users.noreply.github.com>
2024-11-28 00:54:27 +0200

    fix: fix bugs with unit testing (#70)

--- src/controllers/auth/accountRecoveryController.js
+++ src/controllers/auth/accountRecoveryController.js
@@ -6,7 +6,7 @@ const userServices = require("../../services/userService");
 const mails = require("../../utils/mailTemplate");
 const sendEmail = require("../../utils/sendEmail");
 const catchAsync = require("../../utils/catchAsync");
-const manageSessionForUser = require("../../utils/sessionManagement").default;
+const manageSessionForUser = require("../../utils/sessionManagement");
 
 const AppError = require("../../errors/appError");
 
@@ -144,7 +144,6 @@ const resendResetToken = catchAsync(async (req, res, next) => {
 });
 
 const logOutFromAllDevices = catchAsync(async (req, res, next) => {
-  console.log(1);
   const currentAccessToken =
     req.cookies[process.env.COOKIE_ACCESS_NAME] ||
     req.headers.authorization?.split(" ")[1] ||
