5f4ed0e30b14258fe99f51f594914d5908927f79
Mostafa Rabie <mostafamohammed27.mm@gmail.com>
2024-11-27 21:47:39 +0200

    feat: add create member and create admin functions

--- src/services/groupService.js
+++ src/services/groupService.js
@@ -3,8 +3,17 @@ const {Group, GroupAdmin, GroupMember} = require("../models/groupModel");
 const createGroup = (groupName, ownerId) => {
   const admin = new GroupAdmin({
     adminId: ownerId,
+    joinedAt: Date.now(),
     customTitle: "Owner",
+    superAdminId: ownerId,
+    permissions: {
+      postStories: false,
+      editStories: false,
+      deleteStories: false,
+      remainAnonymous: false,
+    },
   });
+
   const newGroup = {
     name: groupName,
     ownerId,
@@ -19,8 +28,29 @@ const findGroupById = (groupId) => {
   return Group.findById(groupId);
 };
 
+const createAdmin = (memberData, newPermission, superAdminId, customTitle) => {
+  const admin = new GroupAdmin({
+    adminId: memberData.memberId,
+    joinedAt: memberData.joinedAt,
+    leftAt: memberData.leftAt,
+    superAdminId,
+    mute: memberData.mute,
+    muteUntil: memberData.muteUntil,
+    customTitle: customTitle ?? "Admin",
+    permissions: newPermission,
+  });
+  return admin;
+};
+
+const createMember = (memberId) => {
+  const member = new GroupMember({memberId});
+  return member;
+};
+
 module.exports = {
   createGroup,
   findGroupById,
   deleteGroup,
+  createAdmin,
+  createMember,
 };
