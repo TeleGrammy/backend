1fa97943c986a8a2880d85cc556babae2d73559f
Mostafa Rabie <mostafamohammed27.mm@gmail.com>
2024-11-29 18:47:45 +0200

    refactor: implement add new member, leave group, delete group via Socket.io events instead of traditional HTTP API

--- src/controllers/group/groupController.js
+++ src/controllers/group/groupController.js
@@ -25,66 +25,6 @@ const addNewGroup = catchAsync(async (req, res, next) => {
   res.status(201).json({group: groupData});
 });
 
-const leaveGroup = catchAsync(async (req, res, next) => {
-  const {groupId} = req.params;
-  const userId = req.user.id;
-
-  const group = await groupService.findGroupById(groupId);
-
-  if (!group) throw new AppError("Group not found.", 404);
-
-  let index = group.members.findIndex((member) =>
-    member.memberId.equals(userId)
-  );
-
-  if (index === -1) {
-    index = group.admins.findIndex((admin) => admin.adminId.equals(userId));
-    if (index === -1)
-      throw new AppError(
-        "User not found. The user is not a member of the group with that id.",
-        404
-      );
-    else group.admins.splice(index, 1);
-  } else {
-    group.members.splice(index, 1);
-  }
-
-  const totalMembers = group.admins.length + group.members.length;
-
-  if (totalMembers === 0) {
-    await groupService.deleteGroup(groupId);
-  } else {
-    await group.save();
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {},
-    message: "You left the group successfully",
-  });
-});
-
-const deleteGroup = catchAsync(async (req, res, next) => {
-  const {groupId} = req.params;
-  const userId = req.user.id;
-  const group = await groupService.findGroupById(groupId);
-
-  if (!group) throw new AppError("Group not found.", 404);
-
-  if (!group.ownerId.equals(userId))
-    throw new AppError(
-      "The user doesn't have the permission to delete the group",
-      403
-    );
-
-  await groupService.deleteGroup(groupId);
-  return res.status(200).json({
-    status: "success",
-    data: {},
-    message: "The group is deleted Successfully",
-  });
-});
-
 const addAdmin = catchAsync(async (req, res, next) => {
   const {groupId} = req.params;
   const {userId} = req.params;
@@ -190,71 +130,6 @@ const removeAdmin = catchAsync(async (req, res, next) => {
     },
     message:
       "The user removed successfully from administrator list and added to members list.",
-  });
-});
-
-const addMember = catchAsync(async (req, res, next) => {
-  const {groupId} = req.params;
-  const {userId} = req.params;
-  const participantId = req.user.id;
-
-  const group = await groupService.findGroupById(groupId);
-  if (!group) throw new AppError("Group not found", 404);
-
-  let participantData = group.members.find((member) =>
-    member.memberId.equals(participantId)
-  );
-
-  const participantType = participantData ? "member" : "admin";
-  participantData =
-    participantData ??
-    group.admins.find((admin) => admin.adminId.equals(participantId));
-
-  if (!participantData)
-    throw new AppError(
-      "Unauthorized Access. The user who did the request is not a member of the group",
-      401
-    );
-
-  const addUsersGroupPermission = group.groupPermission.addUsers;
-
-  if (
-    (!addUsersGroupPermission && participantType === "member") ||
-    (!addUsersGroupPermission &&
-      participantType === "admin" &&
-      !participantData.permissions.addUsers)
-  )
-    throw new AppError(
-      `Insufficient Permissions. The ${participantType} does not have permission to add new users.`,
-      403
-    );
-
-  let index = group.members.findIndex((member) =>
-    member.memberId.equals(userId)
-  );
-
-  if (index === -1) {
-    index = group.admins.findIndex((admin) => admin.adminId.equals(userId));
-    if (index !== -1) throw new AppError("The user is already an admin", 400);
-  } else {
-    throw new AppError("The user is already member of the group", 400);
-  }
-
-  const newMember = groupService.createMember(userId);
-
-  index = group.leftMembers.findIndex((member) =>
-    member.memberId.equals(userId)
-  );
-
-  newMember.leftAt = index === -1 ? null : group.leftMembers[index].leftAt;
-
-  group.members.push(newMember);
-  await group.save();
-
-  res.status(201).json({
-    status: "success",
-    data: {group},
-    message: "The user is added successfully to the group",
   });
 });
 
@@ -464,11 +339,8 @@ const updateMemberPermission = catchAsync(async (req, res, next) => {
 module.exports = {
   addNewGroup,
   findGroup,
-  leaveGroup,
-  deleteGroup,
   addAdmin,
   removeAdmin,
-  addMember,
   updateGroupLimit,
   updateGroupType,
   membersList,
--- src/routes/group/groupRoutes.js
+++ src/routes/group/groupRoutes.js
@@ -5,18 +5,14 @@ const isAuthenticated = require("../../middlewares/isAuthenticated");
 const router = express.Router();
 
 router.route("/").post(isAuthenticated, groupController.addNewGroup);
-router
-  .route("/:groupId")
-  .get(groupController.findGroup)
-  .delete(isAuthenticated, groupController.deleteGroup);
-router
-  .route("/:groupId/participants")
-  .delete(isAuthenticated, groupController.leaveGroup);
+
+router.route("/:groupId").get(groupController.findGroup);
 
 router
   .route("/:groupId/admins/:userId")
   .patch(isAuthenticated, groupController.addAdmin)
   .delete(isAuthenticated, groupController.removeAdmin);
+
 router
   .route("/:groupId/users/:userId")
   .patch(isAuthenticated, groupController.addMember);
@@ -44,4 +40,5 @@ router
 router
   .route("/:groupId/members/:memberId/permissions")
   .patch(isAuthenticated, groupController.updateMemberPermission);
+
 module.exports = router;
