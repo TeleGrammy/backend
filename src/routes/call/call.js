d3e34cd3945a7750a5168f9d2738fdd8bf852ee5
ali bahr <alibahr313@gmail.com>
2024-12-14 21:41:43 +0200

    run linter

--- src/routes/call/call.js
+++ src/routes/call/call.js
@@ -5,6 +5,7 @@ const isAuth = require("../../middlewares/isAuthenticated");
 const router = express.Router();
 
 router.get("/", isAuth, getCalls);
+
 router.get("/:chatId", isAuth, getCallsOfChat);
 
 module.exports = router;
