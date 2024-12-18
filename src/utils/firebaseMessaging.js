5bca3ecd811f65ce1e4064c73d18a85eb547dd65
Mo-Naga <mohamed.abuelnaga03@eng-st.cu.edu.eg>
2024-12-18 11:45:15 +0200

    Update firebaseMessaging.js

--- src/utils/firebaseMessaging.js
+++ src/utils/firebaseMessaging.js
@@ -1,9 +1,22 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../telegrammy-firebase.json");
+
+const firebaseCredentials = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Handle newlines
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER,
+  client_x509_cert_url: process.env.FIREBSAE_CLIENT_CERT,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseCredentials),
 });
 /**
  * Sends a push notification to a specific topic.
