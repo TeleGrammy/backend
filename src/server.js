6818063a483dd0d916ae289f0f3ced61f1e06544
Abd El-Rahman Mostafa <abdelrahman.abdeltawab03@eng-st.cu.edu.eg>
2024-12-23 21:57:26 +0200

    [🥵HOTFIX]: Setting the infrastructure for the HTTP and HTTPS servers for the application (#246)
    * [🥵HOTFIX]: Fix the user's related chat retrieval for the searching for a text to match
    
    * [🥵HOTFIX]: Setting the infrastructure for the HTTP and HTTPS servers for the application

--- src/server.js
+++ src/server.js
@@ -1,16 +1,35 @@
+const fs = require("fs");
 const mongoose = require("mongoose");
-const {createServer} = require("node:http");
+// const {createServer} = require("node:http");
+const http = require("http");
 const app = require("./expressApp");
 const firebaseMessaging = require("./utils/firebaseMessaging");
 
-const httpServer = createServer(app); // app is passed here
-require("./ioApp")(httpServer);
+// Determine if we are in production or development
+let server;
+
+if (process.env.NODE_ENV === "production") {
+  // Production: Use HTTPS
+  const options = {
+    key: fs.readFileSync("/etc/ssl/certs/tls.key"),
+    cert: fs.readFileSync("/etc/ssl/certs/tls.crt"),
+  };
+  server = https.createServer(options, app);
+  console.log(":lock: HTTPS enabled for production");
+} else {
+  // Development: Use HTTP
+  server = http.createServer(app);
+  console.log(":tools: HTTP enabled for development");
+}
+
+// Initiallize the socket server to be compatible with the HTTP/HTTPS initialized servers
+require("./ioApp")(server);
 
 mongoose
   .connect(process.env.DB_HOST)
   .then(() => {
     console.log("Database Connection succeeded");
-    httpServer.listen(
+    server.listen(
       process.env.PORT || 3000,
       process.env.HOST_NAME || "localhost",
       () => {
@@ -25,4 +44,4 @@ mongoose
     console.error("Database connection error:", error);
   });
 
-module.exports = httpServer;
+module.exports = server;
